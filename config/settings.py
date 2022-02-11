29ebfd5e510713148f21511cfac10c3b02ab6f1d
이광현 [brooklyn] <brooklyn@nexon.co.kr>
2022-02-11 17:09:57 +0900

    allow host *

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x%2)d$48kl48oegs+d+!2f*q*g14$lr_3q&a2j=3qh1dt$uv0=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
