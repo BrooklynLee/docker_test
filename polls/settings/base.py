a8986f9f4b56c528ffeb56a4e85bb7b06340c5a3
이광현 [brooklyn] <brooklyn@nexon.co.kr>
2022-02-15 19:49:12 +0900

    environment variable2

--- polls/settings/base.py
+++ polls/settings/base.py
@@ -5,17 +5,15 @@ import environ
 from pathlib import Path
 
 
+# # Set the project base directory
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-# # Set the project base directory
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
